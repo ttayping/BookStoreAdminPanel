15a88907c7efb6ec5e14b8639fcc144ad224c975
ElcinIskenderov <iskenderovelcin@yandex.ru>
2023-04-27 22:53:57 +0400

    Elcin 0.3

---
+++ src/main/java/com/example/bookstoreadminpanel/controller/AuthorController.java
@@ -0,0 +1,42 @@
+package com.example.bookstoreadminpanel.controller;
+
+import com.example.bookstoreadminpanel.dao.entity.Author;
+import com.example.bookstoreadminpanel.service.AuthorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/author")
+public class AuthorController {
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping("/all")
+    public List<Author> getAllAuthors() {
+        return authorService.getAllAuthors();
+    }
+
+    @GetMapping("/get")
+    public Optional<Author> getAuthorById(@RequestParam Long id) {
+        return authorService.getAuthorById(id);
+    }
+
+    @PostMapping("/add")
+    public Author addAuthor(@RequestBody Author author) {
+        return authorService.addAuthor(author);
+    }
+
+    @DeleteMapping("/delete-by-id")
+    public void deleteAuthorById(@RequestParam Long id) {
+        authorService.deleteAuthorById(id);
+    }
+
+    public Author updateAuthor(@RequestBody Author author) {
+        return authorService.updateAuthor(author);
+    }
+}
--- src/main/java/com/example/bookstoreadminpanel/service/impl/AuthorServiceImpl.java
+++ src/main/java/com/example/bookstoreadminpanel/service/impl/AuthorServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.bookstoreadminpanel.service.impl;
 
+import com.example.bookstoreadminpanel.dao.entity.Author;
 import com.example.bookstoreadminpanel.dao.repository.AuthorRepository;
 import com.example.bookstoreadminpanel.service.AuthorService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AuthorServiceImpl implements AuthorService {
@@ -10,5 +14,38 @@ public class AuthorServiceImpl implements AuthorService {
 
     public AuthorServiceImpl(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    @Override
+    public Optional<Author> getAuthorById(Long id) {
+        return authorRepository.findById(id);
+    }
+
+    @Override
+    public Author addAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+        authorRepository.deleteById(id);
+    }
+
+    @Override
+    public Author updateAuthor(Author author) {
+        Optional<Author> resultAuthor = authorRepository.findById(author.getId());
+        if(resultAuthor.isPresent()){
+            resultAuthor.get().setName(author.getName());
+            resultAuthor.get().setBooks(author.getBooks());
+            resultAuthor.get().setId(author.getId());
+            resultAuthor.get().setUpdateDate(author.getUpdateDate());
+            resultAuthor.get().setCreateDate(author.getCreateDate());
+        }
+        return authorRepository.save(resultAuthor.get());
     }
 }
